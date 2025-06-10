b37aead270c88b041ce6b370047079e22b8b37a4
iliyatr20032003 <45252028+iliyatr20032003@users.noreply.github.com>
2025-06-11 01:56:36 +0300

    Revert "Add Buckshot Roulette layout"

--- tests/test_step1.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-const { Shell, ShellType } = require('../js/buckshotCore');
-
-for (let i = 0; i < 10; i++) {
-  const type = i % 2 === 0 ? ShellType.LIVE : ShellType.BLANK;
-  const shell = new Shell(type);
-  assert.strictEqual(shell.type, type);
-}
-console.log('Step 1: Shell type test passed');
